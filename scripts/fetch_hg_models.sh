7f418f9dce535527239e9ac1eadf3e4cabd041cb
ywchao <ywchao@umich.edu>
2019-01-28 14:24:57 -0800

    Update links

--- scripts/fetch_hg_models.sh
+++ scripts/fetch_hg_models.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )/exp"
 mkdir -p $DIR && cd $DIR
 
 FILE=precomputed_hg_models.tar.gz
-URL=http://napoli18.eecs.umich.edu/public_html/data/cvpr_2017/precomputed_hg_models.tar.gz
+ID=1uGXXQx6bAkX9ykqdBNLvlsD_WuUxhk2N
 
 if [ -f $FILE ]; then
   echo "File already exists..."
@@ -13,7 +13,7 @@ fi
 
 echo "Downloading precomputed hg models (50M)..."
 
-wget $URL -O $FILE
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&id=$ID" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p')&id=$ID" -O $FILE && rm -rf /tmp/cookies.txt
 
 echo "Unzipping..."
 
